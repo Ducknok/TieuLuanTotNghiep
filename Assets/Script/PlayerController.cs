743c1d9709e7f7b872e534aed2f3ce6de633c002
Trần Minh Đức <duc8707@gmail.com>
2024-09-18 13:36:40 +0700

    player can slide wall and jump at a certain height

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,23 +8,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField] protected Rigidbody2D rb;
     [SerializeField] protected Animator anim;
     [SerializeField] protected Transform groundCheck;
+    [SerializeField] protected Transform wallCheck;
     [SerializeField] protected LayerMask whatIsGround;
 
     [Header("Movement")]
-    [SerializeField] protected bool isFacingRight = true;
     [SerializeField] protected float movementImputDirection;
     [SerializeField] protected float movementSpeed = 10.0f;
     [SerializeField] protected bool isWalking;
-    
+    [SerializeField] protected bool isFacingRight = true;
 
     [Header("Jump")]
     [SerializeField] protected float jumpForce = 16.0f;
-    [SerializeField] protected float groundCheckRadius;
+    [SerializeField] protected float groundCheckRadius;   
     [SerializeField] protected int amountOfJumps = 1;
     [SerializeField] protected int amountOfJumpsLeft;
     [SerializeField] protected bool isGroundCheck;
     [SerializeField] protected bool canJump;
-    
+
+    [Header("Wall Check")]
+    [SerializeField] protected float wallCheckDistance;
+    [SerializeField] protected float wallSlidingSpeed;
+    [SerializeField] protected float moveForeceInAir;
+    [SerializeField] protected float airDragMultiplier = 0.95f;
+    [SerializeField] protected float variableJumpHeightmultiplier = 0.5f;
+    [SerializeField] protected bool isTouchingWall;
+    [SerializeField] protected bool isWallSliding;
     
 
 
@@ -42,15 +50,29 @@ public class PlayerController : MonoBehaviour
         this.CheckMovementDirection();
         this.UpdateAnimation();
         this.CheckIfCanJump();
+        this.CheckIfWallSliding();
     }
     protected virtual void FixedUpdate()
     {
         this.ApplyMovement();
         this.CheckSurroundings();
     }
+    protected virtual void CheckIfWallSliding()
+    {
+        if(this.isTouchingWall && !this.isGroundCheck && this.rb.velocity.y < 0)
+        {
+            this.isWallSliding = true;
+        }
+        else
+        {
+            this.isWallSliding = false;
+        }
+    }
     protected virtual void CheckSurroundings()
     {
         this.isGroundCheck = Physics2D.OverlapCircle(this.groundCheck.position, this.groundCheckRadius, this.whatIsGround);
+
+        isTouchingWall = Physics2D.Raycast(this.wallCheck.position, this.transform.right, this.wallCheckDistance, this.whatIsGround);
     }
     protected virtual void CheckIfCanJump()
     {
@@ -91,6 +113,7 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("isWalking", this.isWalking);
         anim.SetBool("isGrounded", this.isGroundCheck);
         anim.SetFloat("yVelocity", this.rb.velocity.y);
+        anim.SetBool("isWallSliding", this.isWallSliding);
     }
     protected virtual void CheckInput()
     {
@@ -99,6 +122,10 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             this.Jump();
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            this.rb.velocity = new Vector2(this.rb.velocity.x, this.rb.velocity.y * this.variableJumpHeightmultiplier);
         }
     }
     protected virtual void Jump()
@@ -112,15 +139,46 @@ public class PlayerController : MonoBehaviour
     }
     protected virtual void ApplyMovement()
     {
-        this.rb.velocity = new Vector2(this.movementSpeed * this.movementImputDirection, this.rb.velocity.y);
+        if (this.isGroundCheck)
+        {
+            this.rb.velocity = new Vector2(this.movementSpeed * this.movementImputDirection, this.rb.velocity.y);
+        }
+        else if (!this.isGroundCheck && !this.isWallSliding && this.movementImputDirection != 0)
+        {
+            Vector2 forceToAdd = new Vector2(this.moveForeceInAir * this.movementImputDirection, 0);
+            this.rb.AddForce(forceToAdd);
+
+            if(Mathf.Abs(this.rb.velocity.x) > this.movementSpeed)
+            {
+                this.rb.velocity = new Vector2(this.movementSpeed * this.movementImputDirection, this.rb.velocity.y);
+            }
+        }
+        else if(!this.isGroundCheck && !this.isWallSliding && this.movementImputDirection == 0)
+        {
+            this.rb.velocity = new Vector2(this.rb.velocity.x * this.airDragMultiplier, this.rb.velocity.y);
+        }
+
+        if (this.isWallSliding)
+        {
+            if(this.rb.velocity.y < -this.wallSlidingSpeed)
+            {
+                this.rb.velocity = new Vector2(this.rb.velocity.x, -this.wallSlidingSpeed);
+            }
+        }
     }
     protected virtual void Flip()
     {
-        this.isFacingRight = !this.isFacingRight;
-        this.transform.parent.Rotate(0.0f, 180.0f, 0.0f);
+        if (!this.isWallSliding)
+        {
+            this.isFacingRight = !this.isFacingRight;
+            this.transform.parent.Rotate(0.0f, 180.0f, 0.0f);
+        }
+        
     }
     protected void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(this.groundCheck.position, this.groundCheckRadius);
+
+        Gizmos.DrawLine(this.wallCheck.position, new Vector3(this.wallCheck.position.x + this.wallCheckDistance, this.wallCheck.position.y, this.wallCheck.position.z));
     }
 }
