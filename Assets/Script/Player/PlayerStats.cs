d699702ee4cbf75e6927f833214df60ea0b9bed0
Trần Minh Đức <duc8707@gmail.com>
2024-09-28 16:17:31 +0700

    fix some bugs for player

--- Assets/Script/Player/PlayerStats.cs
+++ Assets/Script/Player/PlayerStats.cs
@@ -11,23 +11,25 @@ public class PlayerStats : MonoBehaviour
         deathChunkParticle,
         deathBloodParticle;
     [SerializeField] protected Image healthImg;
+    [SerializeField] protected GameObject hitPariticle;
     [SerializeField] public float maxHealth;
     [SerializeField] public float currentHealth;
     protected virtual void Start()
     {
-        
         this.playerCtrl = this.transform.GetComponent<PlayerController>();
         this.currentHealth = this.maxHealth;
+        //this.healthImg.fillAmount = currentHealth / 100;
     }
     protected virtual void Update()
     {
-        this.healthImg.fillAmount = currentHealth / 100;
+        Debug.Log(this.transform.position);
     }
     public virtual void DecreaseHealth(float amount)
     {
         this.currentHealth -= amount;
-        
-        if (this.currentHealth <= 0.0f)
+        this.healthImg.fillAmount = currentHealth / 100;
+        Instantiate(this.hitPariticle, this.transform.position, Quaternion.Euler(0f, 180f, Random.Range(0f, 360f)));
+        if(this.currentHealth <= 0.0f)
         {
             this.Die();
         }
