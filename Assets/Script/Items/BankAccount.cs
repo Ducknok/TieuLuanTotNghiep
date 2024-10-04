61441c0111ab938ff773c5d4c4bae8f11d06d942
Trần Minh Đức <duc8707@gmail.com>
2024-10-04 22:28:11 +0700

    remake map 1 and add soul point

--- Assets/Script/Items/BankAccount.cs
+++ Assets/Script/Items/BankAccount.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class BankAccount : MonoBehaviour
 {
     [SerializeField] protected static BankAccount instance;
     public static BankAccount Instance => instance;
-    [SerializeField] protected float bank;
-    [SerializeField] protected Text bankText;
+    [SerializeField] protected float coinBank;
+    [SerializeField] protected float soulBank;
+    [SerializeField] protected TextMeshProUGUI coinText;
+    [SerializeField] protected TextMeshProUGUI soulText;
     
     protected virtual void Start()
     {
-        this.bankText.text = "x " + this.bank.ToString();
+        this.coinText.text = "x " + this.coinBank.ToString();
     }
     protected virtual void Awake()
     {
@@ -23,8 +26,12 @@ public class BankAccount : MonoBehaviour
     }
     public virtual void Money(float cashCollected)
     {
-        this.bank += cashCollected;
-        this.bankText.text = "x " + this.bank.ToString();
+        this.coinBank += cashCollected;
+        this.coinText.text = "x " + this.coinBank.ToString();
     }
-
+    public virtual void Soul(float cashCollected)
+    {
+        this.soulBank += cashCollected;
+        this.soulText.text = "x " + this.soulBank.ToString();
+    }
 }
